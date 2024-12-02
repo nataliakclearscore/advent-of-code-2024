cc468a2d35ac075e9cb54aa09e698a8eb8c2511b
natalia.klyuchnikova <natalia.klyuchnikova@clearscore.com>
2024-12-02 22:59:43 +0000

    One part 2

--- src/main/scala/com/advent/One.scala
+++ src/main/scala/com/advent/One.scala
@@ -2,11 +2,26 @@ package com.advent
 
 import scala.math.abs
 
+
 class One {
-  def process(input: String): Int = {
+
+  private def parseLists(input: String) = {
     val lines = input.split("\n").toList.map(_.trim.split("   ").map(_.toInt))
     val list1 = lines.map(_(0))
     val list2 = lines.map(_(1))
+    (list1, list2)
+  }
+
+  def part1(input: String): Int = {
+    val (list1: List[Int], list2: List[Int]) = parseLists(input)
     list1.sorted.zip(list2.sorted).map { case (a, b) => abs(a - b) }.sum
+  }
+
+  def part2(input: String): Int = {
+    val (list1: List[Int], list2: List[Int]) = parseLists(input)
+    list1.fold(0) { (acc, a) =>
+      val count = list2.count(_ == a)
+      acc + a * count
+    }
   }
 }
--- src/test/scala/com/advent/OneTest.scala
+++ src/test/scala/com/advent/OneTest.scala
@@ -5,13 +5,24 @@ import flatspec._
 import matchers._
 
 class OneTest extends AnyFlatSpec with should.Matchers {
-  "One" should "work for an example" in {
+  "part1" should "work for an example" in {
     val input = """3   4
               4   3
               2   5
               1   3
               3   9
               3   3""".stripMargin
-    new One().process(input) shouldEqual 11
+    new One().part1(input) shouldEqual 11
+  }
+
+  "part2" should "work for an example" in {
+    val input =
+      """3   4
+                4   3
+                2   5
+                1   3
+                3   9
+                3   3""".stripMargin
+    new One().part2(input) shouldEqual 31
   }
 }
