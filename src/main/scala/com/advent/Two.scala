40d51f5c197673853517a22f340bbc236d6a3515
natalia.klyuchnikova <natalia.klyuchnikova@clearscore.com>
2024-12-03 01:06:16 +0000

    Two part 2

--- src/main/scala/com/advent/Two.scala
+++ src/main/scala/com/advent/Two.scala
@@ -7,8 +7,11 @@ class Two {
   private def parseInput(input: String): List[List[Int]] =
     input.split("\n").toList.map(_.trim.split(" ").map(_.toInt).toList)
 
+  private def findDiffs(numbers: List[Int]): List[Int] =
+    numbers.dropRight(1).zip(numbers.tail).map((a, b) => a - b)
+
   private def part1Line(numbers: List[Int]): Boolean = {
-    val diffs = numbers.dropRight(1).zip(numbers.tail).map((a, b) => a - b)
+    val diffs = findDiffs(numbers)
     diffs.forall(abs(_) <= 3) && (diffs.forall(_ < 0) || diffs.forall(_ > 0))
   }
 
@@ -16,4 +19,29 @@ class Two {
     parseInput(input).map(part1Line)
 
   def part1(input: String): Int = part1Lists(input).count(identity)
+
+  private def removeInd(numbers: List[Int], ind: Int): List[Int] =
+    numbers.take(ind) ++ numbers.drop(ind + 1)
+
+  private def part2Line(numbers: List[Int]): Boolean =
+    if (part1Line(numbers)) true
+    else {
+      val diffs = findDiffs(numbers)
+      // find index of the first diff that makes report not safe
+      val ind = diffs.zipWithIndex
+        .find { case (diff, _) =>
+          abs(diff) == 0 || abs(diff) > 3 || diff * diffs.head < 0
+        }
+        .map(_._2)
+        .get
+      // if removing the element around the index makes the report safe, return true
+      part1Line(removeInd(numbers, ind - 1))
+      || part1Line (removeInd(numbers, ind))
+      || part1Line (removeInd(numbers, ind + 1))
+    }
+
+  def part2Lists(input: String): List[Boolean] =
+    parseInput(input).map(part2Line)
+
+  def part2(input: String): Int = part2Lists(input).count(identity)
 }
--- src/test/scala/com/advent/TwoTest.scala
+++ src/test/scala/com/advent/TwoTest.scala
@@ -26,4 +26,48 @@ class TwoTest extends AnyFlatSpec with should.Matchers {
   1 3 6 7 9""".stripMargin
     new Two().part1(input) shouldEqual 2
   }
+
+  "part2Lists" should "work for an example" in {
+    val input =
+      """7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9""".stripMargin
+    new Two().part2Lists(input) shouldEqual List(true, false, false, true, true,
+      true)
+  }
+
+  "part2" should "work for an example" in {
+    val input =
+      """7 6 4 2 1
+  1 2 7 8 9
+  9 7 6 2 1
+  1 3 2 4 5
+  8 6 4 4 1
+  1 3 6 7 9""".stripMargin
+    new Two().part2(input) shouldEqual 4
+  }
+
+  "part2Lists" should "work for line" in {
+    val input: String = "48 46 47 49 51 54 56"
+    new Two().part2Lists(input) shouldEqual List(true)
+  }
+
+  "part2" should "work on edge cases" in {
+    val input =
+      """48 46 47 49 51 54 56
+1 1 2 3 4 5
+1 2 3 4 5 5
+5 1 2 3 4 5
+1 4 3 2 1
+1 6 7 8 9
+1 2 3 4 3
+9 8 7 6 7
+7 10 8 10 11
+29 28 27 25 26 25 22 20""".stripMargin
+    new Two().part2Lists(input) shouldEqual List(true, true, true, true, true,
+      true, true, true, true, true)
+  }
 }
