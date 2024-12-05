bee396d940e2d152e0b11ddd088a3240e0ef458a
natalia.klyuchnikova <natalia.klyuchnikova@clearscore.com>
2024-12-05 13:21:06 +0000

    Five part 2

--- src/main/scala/com/advent/Five.scala
+++ src/main/scala/com/advent/Five.scala
@@ -1,6 +1,8 @@
 package com.advent
 
 class Five {
+
+  // part 1
   def isOrderRight(list: List[Int], rules: List[(Int, Int)]): Boolean = {
     val map: Map[Int, Int] = list.zipWithIndex.toMap
     rules.forall { case (a, b) =>
@@ -11,6 +13,37 @@ class Five {
   }
 
   def part1(lists: List[List[Int]], rules: List[(Int, Int)]): Int = {
-    lists.foldLeft(0)((acc, list) => if (isOrderRight(list, rules)) acc + list(list.length / 2) else acc)
+    lists.foldLeft(0)((acc, list) =>
+      if (isOrderRight(list, rules)) acc + list(list.length / 2) else acc
+    )
+  }
+
+  // part 2
+  def order(list: List[Int], rules: List[(Int, Int)]): List[Int] = {
+    val rulesMap: Map[Int, List[Int]] =
+      rules.groupBy(_._1).map((first, seconds) => (first, seconds.map(_._2)))
+    val arr = list.toArray
+    var i = 1
+    while (i < arr.length) {
+      val bubble = arr(i)
+      var j = i - 1
+      while (j >= 0 && !rulesMap.get(arr(j)).exists(_.contains(bubble))) {
+        val tmp = arr(j)
+        arr(j) = bubble
+        arr(j + 1) = tmp
+        j -= 1
+      }
+      i += 1
+    }
+    arr.toList
+  }
+
+  def part2(lists: List[List[Int]], rules: List[(Int, Int)]): Int = {
+    lists.foldLeft(0)((acc, list) =>
+      if (!isOrderRight(list, rules)) {
+        val ordered = order(list, rules)
+        acc + ordered(ordered.length / 2)
+      } else acc
+    )
   }
 }
--- src/test/scala/com/advent/FiveTest.scala
+++ src/test/scala/com/advent/FiveTest.scala
@@ -101,4 +101,113 @@ class FiveTest extends AnyWordSpec with should.Matchers {
     }
   }
 
+  "order" should {
+    "do nothing for lists the right order" in {
+      val input =
+        """47|53
+          |97|13
+          |97|61
+          |97|47
+          |75|29
+          |61|13
+          |75|53
+          |29|13
+          |97|29
+          |53|29
+          |61|53
+          |97|53
+          |61|29
+          |47|13
+          |75|47
+          |97|75
+          |47|61
+          |75|61
+          |47|29
+          |75|13
+          |53|13
+          |
+          |75,47,61,53,29
+          |97,61,53,29,13
+          |75,29,13
+          |75,97,47,61,53
+          |61,13,29
+          |97,13,75,29,47""".stripMargin
+      val (rules, lists) = parse(input)
+      new Five().order(lists(0), rules) shouldEqual List(75, 47, 61, 53, 29)
+      new Five().order(lists(1), rules) shouldEqual List(97, 61, 53, 29, 13)
+      new Five().order(lists(2), rules) shouldEqual List(75, 29, 13)
+    }
+
+    "order lists that are not in the right order" in {
+      val input =
+        """47|53
+          |97|13
+          |97|61
+          |97|47
+          |75|29
+          |61|13
+          |75|53
+          |29|13
+          |97|29
+          |53|29
+          |61|53
+          |97|53
+          |61|29
+          |47|13
+          |75|47
+          |97|75
+          |47|61
+          |75|61
+          |47|29
+          |75|13
+          |53|13
+          |
+          |75,47,61,53,29
+          |97,61,53,29,13
+          |75,29,13
+          |75,97,47,61,53
+          |61,13,29
+          |97,13,75,29,47""".stripMargin
+      val (rules, lists) = parse(input)
+      new Five().order(lists(3), rules) shouldEqual List(97, 75, 47, 61, 53)
+      new Five().order(lists(4), rules) shouldEqual List(61,29,13)
+      new Five().order(lists(5), rules) shouldEqual List(97,75,47,29,13)
+    }
+  }
+
+  "part2" should {
+    "work for an example" in {
+      val input =
+        """47|53
+          |97|13
+          |97|61
+          |97|47
+          |75|29
+          |61|13
+          |75|53
+          |29|13
+          |97|29
+          |53|29
+          |61|53
+          |97|53
+          |61|29
+          |47|13
+          |75|47
+          |97|75
+          |47|61
+          |75|61
+          |47|29
+          |75|13
+          |53|13
+          |
+          |75,47,61,53,29
+          |97,61,53,29,13
+          |75,29,13
+          |75,97,47,61,53
+          |61,13,29
+          |97,13,75,29,47""".stripMargin
+      val (rules, lists) = parse(input)
+      new Five().part2(lists, rules) shouldEqual 123
+    }
+  }
 }
