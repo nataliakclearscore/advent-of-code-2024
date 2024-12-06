e783a566f3c58ecf5a5371eee1c507f0251e15d8
natalia.klyuchnikova <natalia.klyuchnikova@clearscore.com>
2024-12-06 12:49:19 +0000

    Six part 2

--- src/main/scala/com/advent/Six.scala
+++ src/main/scala/com/advent/Six.scala
@@ -2,22 +2,32 @@ package com.advent
 
 class Six {
 
-  def validIJ(arr: Array[Array[Char]], i: Int, j: Int): Boolean = {
+  def validIJ[T](arr: Array[Array[T]], i: Int, j: Int): Boolean = {
     i >= 0 & j >= 0 & i < arr.length & j < arr(0).length
   }
 
+  def newIJ(i: Int, j: Int, direction: Char) = {
+    val (newI, newJ) = direction match {
+      case 'N' => (i - 1, j)
+      case 'E' => (i, j + 1)
+      case 'S' => (i + 1, j)
+      case 'W' => (i, j - 1)
+    }
+    (newI, newJ)
+  }
+
   def findStartingIJ(arr: Array[Array[Char]]): (Int, Int) = {
-    var strtingI = -1
+    var startingI = -1
     var startingJ = -1
     for (i <- arr.indices) {
       for (j <- arr(0).indices) {
         if (arr(i)(j) == '^') {
-          strtingI = i
+          startingI = i
           startingJ = j
         }
       }
     }
-    (strtingI, startingJ)
+    (startingI, startingJ)
   }
 
   def move(
@@ -26,12 +36,7 @@ class Six {
       j: Int,
       direction: Char
   ): (Int, Int, Char) = {
-    val (newI, newJ) = direction match {
-      case 'N' => (i - 1, j)
-      case 'E' => (i, j + 1)
-      case 'S' => (i + 1, j)
-      case 'W' => (i, j - 1)
-    }
+    val (newI: Int, newJ: Int) = newIJ(i, j, direction)
     if (validIJ(arr, newI, newJ) && arr(newI)(newJ) == '#') {
       val newDirection = directions((directions.indexOf(direction) + 1) % 4)
       (i, j, newDirection)
@@ -53,5 +58,67 @@ class Six {
       direction = newDirection
     }
     arr
+  }
+
+  def isLooped(
+      arr: Array[Array[String]],
+      startingI: Int,
+      startingJ: Int,
+      startingDirection: Char
+  ): Boolean = {
+    var (i, j) = (startingI, startingJ)
+    var direction = startingDirection
+    var looped = false
+    while (validIJ(arr, i, j) && !looped) {
+      if (arr(i)(j).contains(direction.toString)) { // been here before moving in this direction
+        looped = true
+      }
+      arr(i)(j) = arr(i)(j) + direction.toString
+      val (newI: Int, newJ: Int) = newIJ(i, j, direction)
+      if (validIJ(arr, newI, newJ) && arr(newI)(newJ) == "#") {
+        // rotate
+        direction = directions((directions.indexOf(direction) + 1) % 4)
+      } else {
+        // move forward
+        i = newI
+        j = newJ
+      }
+    }
+    looped
+  }
+
+  def part2(
+      original: Array[Array[Char]]
+  ): (Array[Array[Char]], List[(Int, Int)]) = {
+    val arr = original.map(_.clone)
+    val (startingI, startingJ) = findStartingIJ(arr)
+    var (i, j) = (startingI, startingJ)
+    var direction = 'N'
+    var triedObstacles: List[(Int, Int)] = List()
+    var loopingObstacles: List[(Int, Int)] = List()
+    while (validIJ(arr, i, j)) {
+      val (newI: Int, newJ: Int) = newIJ(i, j, direction)
+      if (validIJ(arr, newI, newJ) && arr(newI)(newJ) == '#') { // need to rotate
+        direction = directions((directions.indexOf(direction) + 1) % 4)
+      } else { // can move forward
+        if (
+          validIJ(arr, newI, newJ)
+          && !(newI == startingI && newJ == startingJ)
+          && !triedObstacles.contains((newI, newJ))
+        ) { // not moving out of bounds so obstacle can be here
+          // try placing an obstacle here
+          triedObstacles = (newI, newJ) :: triedObstacles
+          val tryArr = original.map(_.map(_.toString))
+          tryArr(newI)(newJ) = "#"
+          if (isLooped(tryArr, i, j, direction)) {
+            loopingObstacles = (newI, newJ) :: loopingObstacles
+          }
+        }
+        arr(i)(j) = 'X'
+        i = newI
+        j = newJ
+      }
+    }
+    (arr, loopingObstacles)
   }
 }
--- src/test/scala/com/advent/SixTest.scala
+++ src/test/scala/com/advent/SixTest.scala
@@ -25,4 +25,48 @@ class SixTest extends AnyWordSpec with should.Matchers {
       res.foldLeft(0)((acc, row) => acc + row.count(_ == 'X')) shouldEqual 41
     }
   }
+
+  "isLooped" should {
+    "work for an example" in {
+      val input =
+        """....#.....
+          |.........#
+          |..........
+          |..#.......
+          |.......#..
+          |..........
+          |.#.#^.....
+          |........#.
+          |#.........
+          |......#...""".stripMargin
+      val arr: Array[Array[String]] =
+        input.split("\n").map(_.trim.toCharArray.map(_.toString))
+      new Six().isLooped(arr, 6, 4, 'N') shouldBe true
+    }
+  }
+
+  "part2" should {
+    "work for an example" in {
+      val input =
+        """....#.....
+          |.........#
+          |..........
+          |..#.......
+          |.......#..
+          |..........
+          |.#..^.....
+          |........#.
+          |#.........
+          |......#...""".stripMargin
+      val arr: Array[Array[Char]] = input.split("\n").map(_.trim.toCharArray)
+      val (res, obstacles) = new Six().part2(arr)
+
+      res.foreach(row => println(row.mkString))
+      obstacles.foreach((i, j) => res(i)(j) = 'O')
+      println("Obstacles")
+      res.foreach(row => println(row.mkString))
+      println(obstacles.mkString)
+      obstacles.size shouldBe 6
+    }
+  }
 }
