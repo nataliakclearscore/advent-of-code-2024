2fc18b64cf875f6275fb2f7ec543f21a80a6588c
natalia.klyuchnikova <natalia.klyuchnikova@clearscore.com>
2024-12-11 11:07:54 +0000

    Eleven part 2

--- src/main/scala/com/advent/Eleven.scala
+++ src/main/scala/com/advent/Eleven.scala
@@ -2,7 +2,7 @@ package com.advent
 
 class Eleven {
 
-  def blink(input: List[Long]): List[Long] = {
+  def blink1(input: List[Long]): List[Long] = {
     input.flatMap {
       case 0 =>
         List(1)
@@ -14,15 +14,57 @@ class Eleven {
     }
   }
 
-  def blinkN(input: List[Long], times: Int): Int = {
+  def blinkN1(input: List[Long], times: Int): Int = {
     val res = (1 to times).foldLeft(input) { (acc, _) =>
-      blink(acc)
+      blink1(acc)
     }
     res.size
   }
 
   def part1(input: String): Int = {
     val inputList = input.split(" ").map(_.toLong).toList
-    blinkN(inputList, 25)
+    blinkN1(inputList, 25)
+  }
+
+  def blinkN2(
+      input: List[Long],
+      times: Int
+  ): Long = {
+
+    val cache = collection.mutable.Map.empty[(Long, Int), Long] // (input, times) -> result
+
+    def blinkN2(input: Long, times: Int): Long = {
+      if (times == 0) {
+        1L
+      } else if (cache.contains((input, times))) {
+        cache((input, times))
+      } else {
+        val res = input match {
+          case 0 =>
+            blinkN2(1, times - 1)
+          case x if x.toString.length % 2 == 0 =>
+            val split = x.toString.splitAt(x.toString.length / 2)
+            blinkN2(split._1.toInt, times - 1) + blinkN2(
+              split._2.toInt,
+              times - 1
+            )
+          case x =>
+            blinkN2(2024 * x, times - 1)
+        }
+        if (times > 2) cache((input, times)) = res
+        res
+      }
+    }
+
+    var count: Long = 0L
+    for (num <- input) {
+      count += blinkN2(num, times)
+    }
+    count
+  }
+
+  def part2(input: String): Long = {
+    val inputList = input.split(" ").map(_.toLong).toList
+    blinkN2(inputList, 75)
   }
 }
--- src/test/scala/com/advent/ElevenTest.scala
+++ src/test/scala/com/advent/ElevenTest.scala
@@ -7,29 +7,47 @@ class ElevenTest extends AnyWordSpec with should.Matchers {
 
   private val underTest = Eleven()
 
-  "blink" should {
+  "blink1" should {
     "work for an example" in {
       val l0 = List(125, 17).map(_.toLong)
-      val l1 = underTest.blink(l0)
+      val l1 = underTest.blink1(l0)
       l1 shouldEqual List(253000, 1, 7).map(_.toLong)
-      val l2 = underTest.blink(l1)
+      val l2 = underTest.blink1(l1)
       l2 shouldEqual List(253, 0, 2024, 14168).map(_.toLong)
-      val l3 = underTest.blink(l2)
+      val l3 = underTest.blink1(l2)
       l3 shouldEqual List(512072, 1, 20, 24, 28676032).map(_.toLong)
-      val l4 = underTest.blink(l3)
-      val l5 = underTest.blink(l4)
-      val l6 = underTest.blink(l5)
+      val l4 = underTest.blink1(l3)
+      val l5 = underTest.blink1(l4)
+      val l6 = underTest.blink1(l5)
       l6 shouldEqual List(2097446912, 14168, 4048, 2, 0, 2, 4, 40, 48, 2024, 40,
         48, 80, 96, 2, 8, 6, 7, 6, 0, 3, 2).map(_.toLong)
     }
   }
 
-  "blinkN and and blinkNRec part1" should {
+  "blinkN1 and part1" should {
     "work for an example" in {
       val l0 = List(125, 17).map(_.toLong)
-      underTest.blinkN(l0, 6) shouldEqual 22
-      underTest.blinkN(l0, 25) shouldEqual 55312
+      underTest.blinkN1(l0, 6) shouldEqual 22
+      underTest.blinkN1(l0, 25) shouldEqual 55312
       underTest.part1("125 17") shouldEqual 55312
+    }
+  }
+
+  "blinkN2" should {
+    "work for an example 1" in {
+      val l0 = List(125, 17).map(_.toLong)
+      underTest.blinkN2(l0, 6) shouldEqual 22
+    }
+
+    "work for an example 2" in {
+      val l0 = List(125, 17).map(_.toLong)
+      underTest.blinkN2(l0, 25) shouldEqual 55312
+    }
+  }
+
+  "part2" should {
+    "work for an example" in {
+      underTest.part2("125 17") shouldEqual 65601038650482L
     }
   }
 }
