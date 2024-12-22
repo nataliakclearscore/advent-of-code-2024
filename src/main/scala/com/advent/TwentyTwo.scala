9e42c3fd012ff8f98c0073065f5d21c8a32bcc84
natalia.klyuchnikova <natalia.klyuchnikova@clearscore.com>
2024-12-22 21:04:58 +0000

    TwentyTwo part 2

--- src/main/scala/com/advent/TwentyTwo.scala
+++ src/main/scala/com/advent/TwentyTwo.scala
@@ -37,13 +37,43 @@ class TwentyTwo {
       prices += price
       currentSecret = nextSecret(currentSecret)
     }
-    prices.toList
+    val res = prices.toList
+    res
   }
-  
+
   def changesSequence(prices: List[Int]): List[Int] = {
-    prices.sliding(2).map {
-      case Seq(a, b) =>
+    prices
+      .sliding(2)
+      .map { case Seq(a, b) =>
         b - a
-    }.toList
+      }
+      .toList
+  }
+
+  def buildSequenceMap(prices: List[Int]): Map[String, Int] = {
+    val map = scala.collection.mutable.Map.empty[String, Int]
+    prices.sliding(5).foreach { window =>
+      val key = changesSequence(window).mkString(",")
+      val value = window.last
+      if (!map.contains(key)) {
+        map(key) = value
+      }
+    }
+    map.toMap
+  }
+
+  def bestPrice(secrets: List[Long], n: Int): Int = {
+    val maps = secrets.map(priceSequence(_, n + 1)).map(buildSequenceMap)
+    val allKeys = maps.flatMap(_.keys.toSet).toSet
+    
+    allKeys.foldLeft(0) { (bestPrice, key) =>
+      val values = maps.map(map => map.getOrElse(key, 0))
+      val sum = values.sum
+      if (sum > bestPrice) sum else bestPrice
+    }
+  }
+
+  def part2(secrets: List[Long]): Int = {
+    bestPrice(secrets, 2000)
   }
 }
